c49f556c049060b8c8844508be2e4af655a7a366
Calla Rudolph <callarudolph@gmail.com>
2017-07-14 12:54:53 -0700

    create routes for index and stylist twigs with forms to add new stylists and clients

--- app/app.php
+++ app/app.php
@@ -2,6 +2,7 @@
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Stylist.php";
+    require_once __DIR__."/../src/Client.php";
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon';
     $username = 'root';
@@ -14,28 +15,33 @@
         'twig.path' => __DIR__.'/../views'
     ));
 
+    use Symfony\Component\HttpFoundation\Request;
+    Request::enableHttpMethodParameterOverride();
+
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
-    $app->get("/stylists", function() use ($app) {
-        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
-    });
-
-    $app->post("/stylists", function() use ($app) {
-        $stylist = new Stylist($_POST['name']);
+    $app->post("/", function() use ($app) {
+        $name = $_POST['name'];
+        $stylist = new Stylist($name);
         $stylist->save();
-        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist:getAll()));
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
     });
 
     $app->get("/stylists/{id}", function($id) use ($app) {
         $stylist = Stylist::find($id);
-        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist));
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
     });
 
-    $app->post("/delete_stylists", function() use ($app) {
-        Stylist::deleteAll();
-        return $app['twig']->render('index.html.twig');
+    $app->post("/clients", function() use ($app) {
+        $client_name = $_POST['client_name'];
+        $phone_number = $_POST['phone_number'];
+        $stylist_id = $_POST['stylist_id'];
+        $client = new Client($client_name, $phone_number, $stylist_id, $id = null);
+        $client->save();
+        $stylist = Stylist::find($stylist_id);
+        return $app['twig']->render('stylist.html.twig', array('stylist' => $stylist, 'clients' => $stylist->getClients()));
     });
 
     return $app;
